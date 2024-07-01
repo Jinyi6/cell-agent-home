81709b2c4fa1c92477b2f543f9a6371d99cb673f
jinyi <jyliu@tju.edu.cn>
2024-07-02 00:24:49 +0800

    beta7.1-layout-description

--- docs/.vitepress/theme/components/DemoFrame.vue
+++ docs/.vitepress/theme/components/DemoFrame.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="px-0">
     <v-row>
-      <v-col cols="6">
+      <v-col cols="12" md="6">
         <slot name="description">Description</slot>
       </v-col>
-      <v-col cols="6">
+      <v-col cols="12" md="6">
         <v-card
           border="surface-variant sm opacity-100"
           title="Input"
