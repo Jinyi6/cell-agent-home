9091a5356891b0a151b056ea11742310bf5af60c
jinyi <jyliu@tju.edu.cn>
2024-07-03 16:32:22 +0800

    ga

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -33,5 +33,14 @@ export default defineConfig({
       noExternal: ['vuetify']
     }
   },
-  enhanceAppGA
+  head: [
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-38HM5BF06F' }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-38HM5BF06F');
+    `]
+  ]
 })
